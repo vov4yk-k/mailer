8e3d4fed5407ce692c7fe40a4eae2560c547356a
vova <krukvova@gmail.com>
2019-02-03 13:55:58 +0200

    refactor

--- src/main/java/com/itprosteer/mailer/repository/ClientRepository.java
+++ src/main/java/com/itprosteer/mailer/repository/ClientRepository.java
@@ -1,7 +1,6 @@
 package com.itprosteer.mailer.repository;
 
 import com.itprosteer.mailer.model.Client;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ClientRepository extends MongoRepository<Client, String> {
--- src/main/java/com/itprosteer/mailer/repository/EmailRepository.java
+++ src/main/java/com/itprosteer/mailer/repository/EmailRepository.java
@@ -1,7 +1,6 @@
 package com.itprosteer.mailer.repository;
 
 import com.itprosteer.mailer.model.Email;
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EmailRepository extends MongoRepository<Email, String> {
